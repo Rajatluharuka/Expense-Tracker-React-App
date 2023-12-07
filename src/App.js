09aefc273a91a374d7673ab320ca5ad07cd4e532
Rajatluharuka <rajatluharuka123>
2023-12-07 15:16:03 +0530

    Task 1 updated

--- src/App.js
+++ src/App.js
@@ -1,10 +1,50 @@
 import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
+  const expenses = [
+    {
+      id: "e1",
+      title: "Toilet Paper",
+      amount: 94.12,
+      date: new Date(2020, 7, 14),
+      LocationOfExpenditure: "Bengaluru",
+    },
+    {
+      id: "e2",
+      title: "New TV",
+      amount: 799.49,
+      date: new Date(2021, 2, 12),
+      LocationOfExpenditure: "Chennai",
+    },
+    {
+      id: "e3",
+      title: "Car Insurance",
+      amount: 294.67,
+      date: new Date(2021, 2, 28),
+      LocationOfExpenditure: "Mumbai",
+    },
+    {
+      id: "e4",
+      title: "New Desk (Wooden)",
+      amount: 450,
+      date: new Date(2021, 5, 12),
+      LocationOfExpenditure: "New Delhi",
+    },
+  ];
+
+  const expenseItems = expenses.map((expense) => (
+    <ExpenseItem
+      title={expense.title}
+      amount={expense.amount}
+      date={expense.date}
+      LocationOfExpenditure={expense.LocationOfExpenditure}
+    ></ExpenseItem>
+  ));
+
   return (
     <div>
-     <h2>Let's get started!</h2>
-     <ExpenseItem></ExpenseItem> 
+      <h2>Let's get started!</h2>
+      {expenseItems}
     </div>
   );
 }
